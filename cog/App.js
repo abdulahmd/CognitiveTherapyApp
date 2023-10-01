18d9107a7f3fa862a77797319638d1a8746c7278
Abdullah Ahmed <52929221+abdulahmd@users.noreply.github.com>
2023-09-30 20:19:04 -0500

    j

--- cog/App.js
+++ cog/App.js
@@ -105,11 +105,13 @@ export default function App() {
               <View style={styles.scheduleDay}>
                 <Text style={styles.scheduleHeader}>Sunday</Text>
                 <Text style={styles.activity}>10AM Music Therapy, let's listen to {patientData.favoriteSong}</Text>
+                <Text style={styles.activity}>11 am - 1 pm: Reminiscence Therapy: Create a memory box filled with items related to their past, such as old photographs, letters, or mementos. Discuss these items to stimulate memory recall.</Text>
                 <Text style={styles.activity}>2PM - 3PM Arts and crafts, let's paint a scene from {patientData.favoriteTVShow}</Text>
               </View>
               <View style={styles.scheduleDay}>
                 <Text style={styles.scheduleHeader}>Monday</Text>
                 <Text style={styles.activity}>10AM Music Therapy, let's listen to {patientData.favoriteTVShow} theme today</Text>
+                <Text style={styles.activity}>11 am - 1 pm: Reminiscence Therapy: Create a memory box filled with items related to their past, such as old photographs, letters, or mementos. Discuss these items to stimulate memory recall.</Text>
                 <Text style={styles.activity}>2PM - 3PM Arts and crafts, let's draw the logo of the {patientData.favoriteSportTeam} team</Text>
               </View>
             </View>
@@ -117,13 +119,90 @@ export default function App() {
               <View style={styles.scheduleDay}>
                 <Text style={styles.scheduleHeader}>Tuesday</Text>
                 <Text style={styles.activity}>10AM Music Therapy, let's listen to some {patientData.mood} themed music today</Text>
+                <Text style={styles.activity}>11 am - 1 pm: Reminiscence Therapy: Create a memory box filled with items related to their past, such as old photographs, letters, or mementos. Discuss these items to stimulate memory recall.</Text>
                 <Text style={styles.activity}>2PM - 3PM Arts and crafts, let's get creative today and make any art piece with the color {patientData.favoriteColor}</Text>
               </View>
               <View style={styles.scheduleDay}>
+              <Text style={styles.scheduleHeader}>Wednesday</Text>
+                <Text style = {styles.activity}>10 am - 12 pm: Gardening: If they have a green thumb or used to enjoy gardening, provide potted plants or a small garden space for them to tend to. It can be therapeutic and engaging</Text>
+                <Text style = {styles.activity}>1 pm - 3 pm Cooking or Baking: Simple cooking or baking tasks that they used to enjoy, like making cookies or sandwiches, can engage their senses and memories. Have them cook their favorite foods </Text>
+              </View>
+            </View>
+            <View style={styles.scheduleRow}>
+              <View style={styles.scheduleDay}>
+                <Text style={styles.scheduleHeader}>Thursday</Text>
+                <Text style={styles.activity}>Reading: Read aloud from a book, magazine, or newspaper based on their interests. Even if they can't read independently, listening to a familiar topic can be comforting</Text>
+                <Text style={styles.activity}>Puzzles and Games: Offer puzzles, Sudoku, crossword puzzles, or other brain games that match their cognitive abilities. Choose puzzles related to their interests, such as nature or history</Text>
+                <Text style={styles.activity}>Physical Activities: Gentle exercises like chair yoga or stretching can help maintain mobility and reduce restlessness. Ensure they're safe during physical activities</Text>
+                <Text style={styles.activity}>Nature Walks: If possible, take them for short walks in a garden, park, or around the neighborhood to connect with nature and get fresh air.</Text>
+              </View>
+              <View style={styles.scheduleDay}>
+                <Text style={styles.scheduleHeader}>Friday</Text>
+                <Text style={styles.activity}>Reading: Read aloud from a book, magazine, or newspaper based on their interests. Even if they can't read independently, listening to a familiar topic can be comforting</Text>
+                <Text style={styles.activity}>Puzzles and Games: Offer puzzles, Sudoku, crossword puzzles, or other brain games that match their cognitive abilities. Choose puzzles related to their interests, such as nature or history</Text>
+                <Text style={styles.activity}>Physical Activities: Gentle exercises like chair yoga or stretching can help maintain mobility and reduce restlessness. Ensure they're safe during physical activities</Text>
+                <Text style={styles.activity}>Nature Walks: If possible, take them for short walks in a garden, park, or around the neighborhood to connect with nature and get fresh air.</Text>
+              </View>
+            </View>
+            <View style={styles.centeredScheduleDay}>
+              <View style={styles.scheduleDay}>
+                <Text style={styles.scheduleHeader}>Saturday</Text>
+                <Text style={styles.activity}>Reading: Read aloud from a book, magazine, or newspaper based on their interests. Even if they can't read independently, listening to a familiar topic can be comforting</Text>
+                <Text style={styles.activity}>Puzzles and Games: Offer puzzles, Sudoku, crossword puzzles, or other brain games that match their cognitive abilities. Choose puzzles related to their interests, such as nature or history</Text>
+                <Text style={styles.activity}>Physical Activities: Gentle exercises like chair yoga or stretching can help maintain mobility and reduce restlessness. Ensure they're safe during physical activities</Text>
+                <Text style={styles.activity}>Nature Walks: If possible, take them for short walks in a garden, park, or around the neighborhood to connect with nature and get fresh air.</Text>
+              </View>
+            </View>
+          </View>
+          <Button title="Next Week" onPress={() => setStep(4)} />
+          <View style={styles.spaceBetButts} />
+          <Button title="Back" onPress={() => setStep(1)} />
+        </ScrollView>
+      )}
+
+      {step === 4 && (
+        <ScrollView style={styles.schedule}>
+          <Text style={styles.weekTitle}>Week 2</Text>
+          <View style={styles.scheduleGrid}>
+            <View style={styles.scheduleRow}>
+              <View style={styles.scheduleDay}>
+                <Text style={styles.scheduleHeader}>Sunday</Text>
+                <Text style={styles.activity}>Today, we'll get a visit from a therapy animal. Interaction with pets can have a calming effect.</Text>
+                <Text style={styles.activity}>Provide a simple camera or smartphone with a camera app and encourage them to take pictures of things they find interesting, like flowers, birds, or family members.</Text>
+              </View>
+              <View style={styles.scheduleDay}>
+                <Text style={styles.scheduleHeader}>Monday</Text>
+                <Text style={styles.activity}>Offer sensory activities like scented oils, textured objects, or soft fabrics to engage their sense of touch and smell.</Text>
+                <Text style={styles.activity}>Facilitate social interaction with peers in a supportive group setting. Engage in discussions or group activities based on common interests.</Text>
+              </View>
+            </View>
+            <View style={styles.scheduleRow}>
+              <View style={styles.scheduleDay}>
+                <Text style={styles.scheduleHeader}>Tuesday</Text>
+                <Text style={styles.activity}>Encourage them to share stories from their past or create fictional stories together. This can stimulate creativity and memory.</Text>
+                <Text style={styles.activity}>Use tablets or computers for simple games, interactive apps, or video calls with loved ones. Ensure the technology is user-friendly and matches their capabilities.</Text>
+              </View>
+              <View style={styles.scheduleDay}>
                 <Text style={styles.scheduleHeader}>Wednesday</Text>
-                <Text style={styles.activity}>10AM - 12PM Gardening, let's change things up and tend to the plants today</Text>
-                <Text style={styles.activity}>1PM - 3PM, Baking, make cookies</Text>
+                <Text style={styles.activity}>Create personalized memory books with pictures and stories from their life to trigger memories and provide comfort.</Text>
+                <Text style={styles.activity}>Let's listen to {patientData.favoriteSong}</Text>
                 <Text style={styles.activity}>Reading, let's read the book behind {patientData.favoriteMovie} today</Text>
+              </View>
+            </View>
+            <View style={styles.scheduleRow}>
+              <View style={styles.scheduleDay}>
+                <Text style={styles.scheduleHeader}>Thursday</Text>
+                <Text style={styles.activity}>Reading: Read aloud from a book, magazine, or newspaper based on their interests. Even if they can't read independently, listening to a familiar topic can be comforting</Text>
+                <Text style={styles.activity}>Puzzles and Games: Offer puzzles, Sudoku, crossword puzzles, or other brain games that match their cognitive abilities. Choose puzzles related to their interests, such as nature or history</Text>
+                <Text style={styles.activity}>Physical Activities: Gentle exercises like chair yoga or stretching can help maintain mobility and reduce restlessness. Ensure they're safe during physical activities</Text>
+                <Text style={styles.activity}>Nature Walks: If possible, take them for short walks in a garden, park, or around the neighborhood to connect with nature and get fresh air.</Text>
+              </View>
+              <View style={styles.scheduleDay}>
+                <Text style={styles.scheduleHeader}>Friday</Text>
+                <Text style={styles.activity}>Reading: Read aloud from a book, magazine, or newspaper based on their interests. Even if they can't read independently, listening to a familiar topic can be comforting</Text>
+                <Text style={styles.activity}>Puzzles and Games: Offer puzzles, Sudoku, crossword puzzles, or other brain games that match their cognitive abilities. Choose puzzles related to their interests, such as nature or history</Text>
+                <Text style={styles.activity}>Physical Activities: Gentle exercises like chair yoga or stretching can help maintain mobility and reduce restlessness. Ensure they're safe during physical activities</Text>
+                <Text style={styles.activity}>Nature Walks: If possible, take them for short walks in a garden, park, or around the neighborhood to connect with nature and get fresh air.</Text>
               </View>
             </View>
             <View style={styles.centeredScheduleDay}>
@@ -134,9 +213,8 @@ export default function App() {
               </View>
             </View>
           </View>
-          <Button title="Next Week" onPress={() => setStep(4)} />
           <View style={styles.spaceBetButts} />
-          <Button title="Back" onPress={() => setStep(1)} />
+          <Button title="Back" onPress={() => setStep(3)} />
         </ScrollView>
       )}
     </ScrollView>
