9825d53115dcc2e620758d507a772c7140cd70ef
Abdullah Ahmed <52929221+abdulahmd@users.noreply.github.com>
2023-11-16 18:53:28 -0600

    h

--- cog/App.js
+++ cog/App.js
@@ -229,6 +229,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     padding: 20,
+    backgroundColor: '#00bfff',
   },
   header: {
     fontSize: 24,
